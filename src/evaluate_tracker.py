1b2d55d6837742dcfac0351f70c8c459d6b56d33
Vincenzo <vincenzontt02@gmail.com>
2025-05-31 22:42:23 +0200

    added label argument

--- src/evaluate_tracker.py
+++ src/evaluate_tracker.py
@@ -8,13 +8,14 @@ import glob
 
 def main():
     parser = argparse.ArgumentParser(description="Evaluate detections (not tracking) on every 5th frame")
-    parser.add_argument('--results', type=str, required=True, help='Path to tracking_results.json')
-    parser.add_argument('--video_name', type=str, required=True, help='Name of the video file (without extension)')
+    parser.add_argument('-r','--results', type=str, required=True, help='Path to tracking_results.json')
+    parser.add_argument('-v','--video_name', type=str, required=True, help='Name of the video file (without extension)')
+    parser.add_argument('-l','--label', type=str, required=True, help='Name of the video file (without extension)')
 
     args = parser.parse_args()
 
     # Constants
-    LABEL_DIR = 'data/frames_annotated_yolov8/train/labels'
+    LABEL_DIR = args.label
     NUM_FRAMES = 600
     FRAME_INTERVAL = 5
     IOU_THRESHOLD = 0.5
@@ -40,7 +41,6 @@ def main():
         ground_truth_boxes=yolo_labels,
         tracking_results=detection_results
     )
-    breakpoint()
     print("\nDetection Evaluation Metrics:")
     for k, v in metrics.items():
         print(f"{k}: {v:.4f}" if isinstance(v, float) else f"{k}: {v}")
